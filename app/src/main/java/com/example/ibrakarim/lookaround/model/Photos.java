07613e4c31be3b69df1bc9be6911f336d099595d
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-08-14 22:22:22 +0200

    add places model package

---
+++ app/src/main/java/com/example/ibrakarim/lookaround/model/Photos.java
@@ -0,0 +1,46 @@
+package com.example.ibrakarim.lookaround.model;
+
+class Photos {
+
+    private String photo_reference;
+
+    private String height;
+
+    private String width;
+
+    public String getPhoto_reference ()
+    {
+        return photo_reference;
+    }
+
+    public void setPhoto_reference (String photo_reference)
+    {
+        this.photo_reference = photo_reference;
+    }
+
+    public String getHeight ()
+    {
+        return height;
+    }
+
+    public void setHeight (String height)
+    {
+        this.height = height;
+    }
+
+    public String getWidth ()
+    {
+        return width;
+    }
+
+    public void setWidth (String width)
+    {
+        this.width = width;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [photo_reference = "+photo_reference+", height = "+height+", width = "+width+"]";
+    }
+}
